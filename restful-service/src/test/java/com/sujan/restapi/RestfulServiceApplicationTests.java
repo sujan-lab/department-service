e355f99ea81494e2937982e474221ba4a2971e91
sujan-lab <sujan.gowra@gmail.com>
2021-03-18 15:12:20 -0400

    Rest EMail Send 1.0

--- restful-service/src/test/java/com/sujan/restapi/RestfulServiceApplicationTests.java
+++ restful-service/src/test/java/com/sujan/restapi/RestfulServiceApplicationTests.java
@@ -1,10 +1,12 @@
-package com.sujan.department;
+package com.sujan.restapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
-class DepartmentServiceApplicationTests {
+class RestfulServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
